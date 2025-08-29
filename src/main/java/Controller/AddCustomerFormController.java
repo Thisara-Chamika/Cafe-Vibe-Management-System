43fe22211345b432c15637769e0a0574f23cff25
Thisara-Chamika <thisarachamika2003@gmail.com>
2025-08-29 12:29:06 +0530

    Add Customer Form & integrated Lombok

--- src/main/java/Controller/AddCustomerFormController.java
+++ src/main/java/Controller/AddCustomerFormController.java
@@ -1,5 +1,53 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class AddCustomerFormController {
+
+    @FXML
+    private ComboBox<?> txtCategoryCombo;
+
+    @FXML
+    private TextField txtCustomerAddress;
+
+    @FXML
+    private TextField txtCustomerEmail;
+
+    @FXML
+    private TextField txtCustomerId;
+
+    @FXML
+    private TextField txtCustomerIdforOrder;
+
+    @FXML
+    private TextField txtCustomerName;
+
+    @FXML
+    private ComboBox<?> txtItemCombo;
+
+    @FXML
+    private TextField txtMobileNumber;
+
+    @FXML
+    private TextField txtOrderId;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    private TextField txtUnitPrice;
+
+    @FXML
+    void btnAddCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderOnAction(ActionEvent event) {
+
+    }
 
 }

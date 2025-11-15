3b6cbda804bf5ce76452f8058bc8a5fdbd5b6769
Thisara-Chamika <thisarachamika2003@gmail.com>
2025-11-16 03:42:39 +0530

    Set up the Customer page to the Home page

--- src/main/java/controller/AddCustomerFormController.java
+++ src/main/java/controller/AddCustomerFormController.java
@@ -3,21 +3,20 @@ package controller;
 import dataBase.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import model.Customer;
 
-import java.io.IOException;
 import java.sql.SQLException;
 
 public class AddCustomerFormController {
 
-    @FXML
-    private ComboBox txtCategoryCombo;
-
+    public TableView tblCustomerTable;
+    public TableColumn colCustomerId;
+    public TableColumn colCustomerName;
+    public TableColumn colCustomerAddress;
+    public TableColumn colCustomerEmail;
     @FXML
     private TextField txtCustomerAddress;
 
@@ -28,25 +27,11 @@ public class AddCustomerFormController {
     private TextField txtCustomerId;
 
     @FXML
-    private TextField txtCustomerIdforOrder;
-
-    @FXML
     private TextField txtCustomerName;
 
-    @FXML
-    private ComboBox txtItemCombo;
 
     @FXML
     private TextField txtMobileNumber;
-
-    @FXML
-    private TextField txtOrderId;
-
-    @FXML
-    private TextField txtQty;
-
-    @FXML
-    private TextField txtUnitPrice;
 
 // ------------ Add Customer -----------------
     @FXML
@@ -63,35 +48,6 @@ public class AddCustomerFormController {
         resetTextFields();
     }
 
-    // ------------ View Customers Table -----------------
-    @FXML
-    void btnViewAllCustomersOnAction(ActionEvent event) throws IOException {
-        Stage stage = new Stage();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ViewAllCustomersTable.fxml"))));
-        stage.show();
-    }
-
-    // ------------ Place Order -----------------
-    @FXML
-    void btnPlaceOrderOnAction(ActionEvent event) {
-
-    }
-
-    // ------------ Back to Home page -----------------
-    @FXML
-    void btnHomePageOnAction(ActionEvent event) throws IOException {
-        Stage stage = new Stage();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/HomePage.fxml"))));
-        stage.show();
-    }
-
-    // -------------- View All Orders Table --------------
-    public void btnViewAllOedersOnAction(ActionEvent actionEvent) throws IOException {
-        Stage stage = new Stage();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ViewAllOrders.fxml"))));
-        stage.show();
-    }
-
 // -------------- reset text fields --------------
     public void resetTextFields(){
                 txtCustomerId.setText("");
@@ -99,5 +55,17 @@ public class AddCustomerFormController {
                 txtCustomerAddress.setText("");
                 txtMobileNumber.setText("");
                 txtCustomerEmail.setText("");
+    }
+
+    public void btnUpdateCustomerOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnDeleteCustomerOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnSearchCustomerOnAction(ActionEvent actionEvent) {
+        
     }
 }
--- src/main/java/controller/HomePageFormController.java
+++ src/main/java/controller/HomePageFormController.java
@@ -3,31 +3,86 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HomePageFormController {
 
+    public AnchorPane FormViewPane;
+
     @FXML
     void btnAddCustomerOnAction(ActionEvent event) throws IOException {
-        Stage stage = new Stage();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/AddCustomer.fxml"))));
-        stage.show();
+        URL resource = this.getClass().getResource("/view/AddCustomer.fxml");
+        assert resource != null;
+
+        try {
+            Parent load = FXMLLoader.load(resource);
+            this.FormViewPane.getChildren().clear();
+            this.FormViewPane.getChildren().add(load);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnAddItemsOnAction(ActionEvent event) throws IOException {
-        Stage stage = new Stage();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/AddItems.fxml"))));
-        stage.show();
+        URL resource = this.getClass().getResource("/view/AddItems.fxml");
+        assert resource != null;
+
+        try {
+            Parent load = FXMLLoader.load(resource);
+            this.FormViewPane.getChildren().clear();
+            this.FormViewPane.getChildren().add(load);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnViewAllOrdersOnAction(ActionEvent event) throws IOException {
-        Stage stage = new Stage();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ViewAllOrders.fxml"))));
-        stage.show();
+        URL resource = this.getClass().getResource("/view/ViewAllOrders.fxml");
+        assert resource != null;
+
+        try {
+            Parent load = FXMLLoader.load(resource);
+            this.FormViewPane.getChildren().clear();
+            this.FormViewPane.getChildren().add(load);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void btnViewDashBoardOnAction(ActionEvent actionEvent) {
+        URL resource = this.getClass().getResource("/view/DashBoard.fxml");
+        assert resource != null;
+
+        try {
+            Parent load = FXMLLoader.load(resource);
+            this.FormViewPane.getChildren().clear();
+            this.FormViewPane.getChildren().add(load);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void btnViewReportOnAction(ActionEvent actionEvent) {
+        URL resource = this.getClass().getResource("/view/ViewReport.fxml");
+        assert resource != null;
+
+        try {
+            Parent load = FXMLLoader.load(resource);
+            this.FormViewPane.getChildren().clear();
+            this.FormViewPane.getChildren().add(load);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

9c8f4b551d6ba4d8fdce527a012a473a3648058c
Thisara-Chamika <thisarachamika2003@gmail.com>
2025-11-16 16:24:34 +0530

    Create Item Page UI

--- src/main/java/controller/AddItemsFormController.java
+++ src/main/java/controller/AddItemsFormController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class AddItemsFormController {
+    public TableView tblItem;
+    public TableColumn colItemCode;
+    public TableColumn colItemDescription;
+    public TableColumn colItemQuantity;
+    public TableColumn colItemPrice;
+    public JFXTextField txtItemCode;
+    public JFXTextField txtItemDescription;
+    public JFXTextField txtItemPrice;
+    public JFXTextField txtItemQuantity;
+
+    public void btnAddItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchItemOnAction(ActionEvent actionEvent) {
+    }
 }
